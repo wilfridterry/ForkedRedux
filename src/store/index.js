60cb677cd46fa1818760b2d1b025eff75f35f1da
Stannis <zalevschi1998@gmail.com>
2022-08-22 23:04:56 +0300

    Changed creating store by redux/toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,49 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import heroesReducer from "../reducers/heroesReducer";
 import filtersReducer from "../reducers/filtersReducer";
-import ReduxThunk from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
 const stringMiddleware = () => (next) => (action) => {
   if (typeof action === "string") {
     return next({ type: action });
   }
   return next(action);
-} 
-
-const enhancer = (createStore) => {
-  return (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-      if (typeof action === "string") {
-        return oldDispatch({ type: action });
-      }
-      return oldDispatch(action);
-    };
-    return store;
-  };
 };
 
-const store = createStore(
-  combineReducers({
+// const enhancer = (createStore) => {
+//   return (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//       if (typeof action === "string") {
+//         return oldDispatch({ type: action });
+//       }
+//       return oldDispatch(action);
+//     };
+//     return store;
+//   };
+// };
+
+// const store = createStore(
+//   combineReducers({
+//     heroesReducer,
+//     filtersReducer,
+//   }),
+//   compose(
+//     applyMiddleware(ReduxThunk, stringMiddleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+//
+
+const store = configureStore({
+  reducer: {
     heroesReducer,
     filtersReducer,
-  }), 
-  compose(
-    applyMiddleware(ReduxThunk, stringMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
-//  
+  },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware) 
+});
 
 export default store;
+ 
